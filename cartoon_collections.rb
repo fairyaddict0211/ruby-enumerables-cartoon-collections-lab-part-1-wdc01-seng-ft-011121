3ec4a1a4d4fdf2850d144e90a4a0cb039739759f
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-03 00:04:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def greet_characters(array)
  array.each do |characters|
    puts "Hello #{characters}!"
 end
+end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
