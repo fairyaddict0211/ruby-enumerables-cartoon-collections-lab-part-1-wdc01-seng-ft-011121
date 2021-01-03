4723144297383688a1626dcc8b063d87f8896389
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-03 00:02:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,3 @@ def list_dwarves(array)
 end
 
 
-oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
-oppressed_workers.each do |oppressed_worker|
-   puts "#{oppressed_worker.capitalize} wants to start a union!"
-end
